12ef43a261701d2a3cd2af0ef996c5c4aeeeb8c0
Julie Su <112983786+SeiKAl-SeiKAs@users.noreply.github.com>
2022-11-18 23:52:59 -0500

    E8 super rough draft (figma to be linked)

---
+++ Harmonic_Collection/Entries8/assets/main.js
@@ -0,0 +1,20 @@
+// define mode in var
+//trigger var with audio played once
+//make answer appear by adding class ; mode have divverent div
+//
+//
+let varRunner = 0;
+
+document.getElementById("button-tobe").addEventListerner ("click", function() {
+  getRandomInt(7);
+let resultVar = varRunner + 1;
+console.log("the result :", resultVar);
+
+if (resultVar == 1) {
+  document.getElementById("result-1");
+  element.classlist.add("shown");
+  //so goes on with other mode (draft here)
+
+}
+
+}
