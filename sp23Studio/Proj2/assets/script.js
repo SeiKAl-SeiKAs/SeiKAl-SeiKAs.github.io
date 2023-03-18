be8be9104d82c12f61a48f985b55080c2fa2f19f
Julie Su <112983786+SeiKAl-SeiKAs@users.noreply.github.com>
2023-03-17 22:48:01 -0400

    database import ( proto ver,
     unprocessed file includeed)

---
+++ sp23Studio/Proj2/assets/script.js
@@ -0,0 +1,22 @@
+//fetch json array
+//const array as ref from json
+
+//full display:
+// get array length
+//generate enough div for the content
+// add sect within the div and set the elements
+//add text, style, position (etc)
+
+//generate div base on div template
+
+//insert img(src) content
+//insert text(array[id])
+
+
+
+//ALT: filtering func
+// ref from const array and dele unqualified, 
+//sort with conditions
+// refresh the page 
+// delete the old divs
+//and regenerate the divs
