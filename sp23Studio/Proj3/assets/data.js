2b87d7e26794e4fd0e2655f96d075d5170daa651
Julie Su <112983786+SeiKAl-SeiKAs@users.noreply.github.com>
2023-04-16 00:02:44 -0400

    C2 stu proj 3 progress

---
+++ sp23Studio/Proj3/assets/data.js
@@ -0,0 +1,47 @@
+import data from './pave.json'assert { type: 'json' };
+//const array as ref from json
+
+const pave = data;
+
+
+/* online json ver */
+/* const paveAPI = 'https://data.cityofnewyork.us/resource/6yyb-pb25.json';
+
+fetch (paveAPI + '?$limit=50000') //MARK: limits set
+.then(response => response.json())
+.then(data => {
+pave(data)
+
+}) */
+
+const temp = (data)=>{
+	let Rgood = 0
+	let Rfair = 0
+	let Rpoor = 0
+	let Runknown = 0
+
+	console.log( "something here")
+
+}
+
+//filter idea : dateof date, (don't touch the rate, gacha related)
+
+// define vars
+
+//calc the ave for chart
+
+// insert value into chart
+
+//insert chart effect
+
+
+/* GACHA sys. */
+//draw one & identify
+
+//function result and add ani to icon
+
+//function when redraw (remove prev ani class)
+
+
+/* EVENT LIS    TO THE OPTION list (select active) */
+/* EVENT LIS the gacha button */
