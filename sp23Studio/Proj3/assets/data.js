0bef3cad66c26aa5cbb3dc7c3dc5ce05067c8115
Julie Su <112983786+SeiKAl-SeiKAs@users.noreply.github.com>
2023-04-25 21:03:44 -0400

    ids color update

--- sp23Studio/Proj3/assets/data.js
+++ sp23Studio/Proj3/assets/data.js
@@ -2,16 +2,16 @@
 
 const url = 'https://data.cityofnewyork.us/resource/6yyb-pb25.json';
 
-let arrayLength = 35000;
+let arrayLength = 1000;
 
 //esti change  array.length == 1000;
 
 //MARk: after func design
 /* fetch(url + '?$limit=15000') */ // limit current highest (5000= fast, 15000 = 5sec+)
 
-/* fetch("./assets/pave1000.json") */ // temp smol file (instant)
+fetch("./assets/pave1000.json") // temp smol file (instant)
 /* fetch("./assets/pave15000.json")  */ // local file 2 sec?local
-fetch("./assets/pave35000.json")  // local file (esti 5+min)
+/* fetch("./assets/pave35000.json") */  // local file (esti 5+min)
 /* fetch("./assets/pave.json") */  // local full file  (forever)
 
 //set dataset length;
@@ -19,9 +19,13 @@ fetch("./assets/pave35000.json")  // local file (esti 5+min)
 .then(response => response.json())
 .then(data => {
 	fullData = data;
-	rateProcess(fullData);
+	togClass;
+	setTimeout(	rateProcess(fullData), 3000);
 	console.log(fullData.length);
+	setTimeout(togClass, 2000);
 })
+
+//set async?
 
 //get full array length; (approx 135K-)
 let arrayMax = 133553; //array size (local / api respose would be slow)
@@ -46,25 +50,23 @@ offsetRandom(arrayMax);
 console.log(randomTemp);
 
 
-//back-up arrays
-/* let locData = [];
-const processData = Array.from(locData);
-let yrArray=[] // repeat or name change?
-let tempArray=Array.from(processData)
- */
-
 //declare global var
 let Rgood = 0
 let Rfair = 0
 let Rpoor = 0
 let Runknown = 0
 
+let goodPie = 0
+let fairPie = 0
+let poorPie = 0
+
 const rateProcess = (fullData)=>{ //process full data first
 	/* let Rgood = 0
 	let Rfair = 0
 	let Rpoor = 0
 	let Runknown = 0 */
-								console.log('running... The total count');
+	
+			console.log('running... The total count');
 // run and divide through array
 	fullData.forEach( streetRate =>{
 		if (streetRate.ratinglaye == 'GOOD' ) Rgood++
@@ -79,42 +81,40 @@ const rateProcess = (fullData)=>{ //process full data first
 	console.log( 'Poor:' + Rpoor)
 	console.log( '?state:' + Runknown)
 
-				let dummycharttext = document.getElementById('temp2ndtext');
-				let textDummy = document.createTextNode('Good:' + Rgood + '   Fair:' + Rfair + '   Poor:' + Rpoor + '   ????:' + Runknown)
-					
-				dummycharttext.appendChild(textDummy);
+	goodPie = (100 * Rgood ) / paveLength;
+	console.log ("Good % :" + goodPie + "%");
 
-				//chart.js for ALL# pie chart
-						var chart = new CanvasJS.Chart("chartContainerAll", {
-							animationEnabled: true,
-							backgroundColor: "transparent",
-							title:{
-								text: "Rating overall (proto:15000)",
-								fontFamily:"Mitr",
-								horizontalAlign: "left",
-								fontSize:15
-							},
-							data: [{
-								type: "doughnut",
-								startAngle: 60,
-								innerRadius: 30,
-								indexLabelFontSize: 10,
-								indexLabel: "{label} - #percent%",
-								toolTipContent: "<b>{label}:</b> {y} (#percent%)",
-								indexLabelPlacement: "inside",
-								dataPoints: [
-									{ y: Rgood, label: "Good" },
-									{ y: Rfair, label: "Fair" },
-									{ y: Rpoor, label: "Poor" },
-									{ y: Runknown, label: "Null"},
-									
-								]
-							}]
-						});
-						chart.render();
+	fairPie = (100 * Rfair ) / paveLength;
+	console.log ("Fair % :" + fairPie + "%");
+
+	poorPie = (100 * Rpoor ) / paveLength;
+	console.log ("Poor % :" + poorPie + "%")
 
 
+	//const the id for data insert
+	const goodNumber = document.getElementById("displayW");
+	const fairNumber = document.getElementById("displayF");
+	const poorNumber = document.getElementById("displayFh");
+
+	goodNumber.innerHTML = goodPie + "%";
+	fairNumber.innerHTML = fairPie + "%";
+	poorNumber.innerHTML = poorPie + "%";
+
+	const displayP = document.getElementById("paveLong");
+	const displayA = document.getElementById("arrayLong");
+
+	displayP.innerHTML = paveLength;
+	displayA.innerHTML = arrayMax;
+
+	
+				
 }
+
+const togClass = function loadAniSwitch(){
+	const loader = document.querySelector('.lds-ellipsis');
+	loader.classList.toggle('open');
+}
+
 //copy the value to filter vars
 let FRgood = Rgood
 let FRfair = Rfair
@@ -123,47 +123,37 @@ let FRunknown = Runknown
 
 
 
-/* time filter proto */
-/* let dateTempProcess = dummy; //MARK: replace the array into real ones
-
-let processed= dateTempProcess.filter(filterFunc);
-
- function filterFunc(dateTempProcess.inspection){
-	const startDate = document.getElementById('startDate');
-	const endDate = document.getElementById('endDate');
-	//return date >= "2000-11-04T00:00:00.000" && date <= "2010-12-31T00:00:00.000" ; 
-	return date >= startDate.value && date <= endDate.value ;
-
-
-} 
-console.log(processed); //MARK test line
-
-
-tag = [onnstreetna, fromstreet, tostreetna, manualrati, ratinglaye, inspection];
-timeSeg = ["2000 to 2010", "2011 to 2016" , "2017-2021", " 2022" , "2023" ,] ; 
-// divide seg in 5 parts
- */
+// gene mouseover title.properties (colorblocks)
 
 /* loc, loc-start, loc-end, rate#, rate word, time of record */
 
+function blockGenerate(fullData){ /* MARK */
+		//create div
+		const blockGene = document.createElement("div");
+		//get rate property (good fair poor)
+		const getRate = fullData.ratinglaye;
+	
+		const addGood = classList.add("good");
+		const addFair = classList.add("fair");
+		const addPoor = classList.add("poor");
+		
+		// set the hover.text
+		const hoverTextO = fullData.onstreetna;
+		const hoverTextF = fullData.fromstreet;
+		const hoverTextT = fullData.tostreetna;
+
+		const hoverText = "This segments is on" + hoverTextO + "from" + hoverTextF +  "to" + hoverTextT ;
+
+		const hoverContent = setAttribute("title", hoverText);
+
+		//if else insert class type
+		
+
+} 
+/* MARK -activate? */
+/* fullData.forEach(blockGenerate); */
 
 //filter idea : dateof date, (don't touch the rate, gacha related)
-
-// define vars
-
-//calc the ave for chart
-
-// insert value into chart
-
-//insert chart effect
-
-
-/* GACHA sys. */
-//draw one & identify
-
-//function result and add ani to icon
-
-//function when redraw (remove prev ani class)
 
 
 /* EVENT LIS    TO THE OPTION list (select active) */
